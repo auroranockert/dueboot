ac2185b5fa12f54f210bffb7004d212e6230162e
Steve Jenson <stevej@fruitless.org>
2015-01-08 09:42:45 -0800

    Changes for Rust Nightly c7dd3c4d6. Making whitespace conform to style guideline. Have the LED flash a distinct pattern for easier success detection'

--- core.rs
+++ core.rs
@@ -1,13 +1,19 @@
 #![feature(asm)]
+#![feature(lang_items)]
 
-#![allow(ctypes)]
 #![no_std]
 
-#![crate_id = "blinky#0.1"]
-
 use arduino::{init, delay, pinMode, digitalWrite, analogWrite, LOW, HIGH, OUTPUT};
-
 mod arduino;
+
+#[lang="sized"]
+trait Sized {}
+
+#[lang="copy"]
+trait Copy {}
+
+#[lang="sync"]
+trait Sync {}
 
 static PWM:u32 = 2;
 static LED:u32 = 13;
@@ -17,18 +23,25 @@ static PWM_HIGH:u32 = 16;
 
 #[no_mangle]
 pub fn main() {
-	init();
-	delay(1);
-	pinMode(LED, OUTPUT);
-	digitalWrite(LED, LOW);
-	analogWrite(PWM, PWM_LOW);
+    init();
+    delay(1);
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, LOW);
+    analogWrite(PWM, PWM_LOW);
 
-	loop {
-		analogWrite(PWM, PWM_HIGH);
-		digitalWrite(LED, HIGH);
-		delay(100);
-		analogWrite(PWM, PWM_LOW);
-		digitalWrite(LED, LOW);
-		delay(900);
-	}
+    loop {
+        analogWrite(PWM, PWM_HIGH);
+        digitalWrite(LED, HIGH);
+        delay(100);
+        analogWrite(PWM, PWM_LOW);
+        digitalWrite(LED, LOW);
+        delay(100);
+
+        analogWrite(PWM, PWM_HIGH);
+        digitalWrite(LED, HIGH);
+        delay(1000);
+        analogWrite(PWM, PWM_LOW);
+        digitalWrite(LED, LOW);
+        delay(1000);
+  }
 }
